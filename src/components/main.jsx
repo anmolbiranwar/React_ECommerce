a0517657f5c854262c58ce1c460293540984b05d
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-10-07 13:34:05 -0700

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main6.jpg"
+            src="./assets/main1.jpg"
             alt="Card"
             height={100}
           />
