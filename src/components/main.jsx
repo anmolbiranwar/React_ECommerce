9e62790b2cff05bb5644d44de59eb64589dc79ee
anmolbiranwar <83487792+anmolbiranwar@users.noreply.github.com>
2023-10-12 18:17:05 -0700

    Update main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -7,20 +7,20 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main3.jpg"
+            src="./assets/main.jpg"
             alt="Card"
-            style={{height:'550px'}}
+            style={{height:'440px'}}
           />
-          <div className="card-img-overlay d-flex align-items-end">
-            <div className="container">
-              <h3 className="card-title fs-1 text fw-lighter">New Season Arrivals</h3>
-              <p className="card-text fs-5 d-none d-sm-block ">
+          {/* <div className="card-img-overlay d-flex align-items-center">
+            <div className="container p-3">
+              <h3 className="card-title fs-1  fw-bold">New Season Arrivals</h3>
+              <p className="card-text fs-5  fw-bold d-none d-sm-block ">
                 This is a wider card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
               </p>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
